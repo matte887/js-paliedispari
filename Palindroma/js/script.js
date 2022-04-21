2f855d30a2fbc641d3ce9db30286e19fc3ba407a
matte887 <m.erroi92@gmail.com>
2022-04-21 14:37:56 +0200

    Palindroma: il programma riconosce i palindromi

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,19 +1,26 @@
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma.
 
-// Chiedo all'utente di inserire una parola
+// Chiedo all'utente di inserire una parola.
 const userWord = prompt("Inserisci una parola");
-console.log(userWord);
+console.log(`La parole inserita è "${userWord}"`);
 
-// Trasformo la parola dell'utente, nella parola letta da destra a sinistra
+// Trasformo la parola dell'utente, nella parola speculare.
 let invUserWord = "";
 
 for (let i = userWord.length - 1; i >= 0; i--) {
     invUserWord += userWord[i];
-    console.log(userWord[i]);    
+    console.log(`La lettera ${[i + 1]} è ${userWord[i]}`);    
 }
 
-console.log(invUserWord);
+// Stampo il risultato
+console.log(`La parola invertita è "${invUserWord}"`);
+
+if (userWord === invUserWord) {
+    console.log(`"${userWord}" è un palindromo!`);
+} else {
+    console.log(`"${userWord}" non è un palindromo...`);
+}
 
 // FUNZIONE
 // function isThisAPalindrom(userWord) {
